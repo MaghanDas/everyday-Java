23dbe564c3328616d5885ff390bfdf1d008627ba
maghandas <dasmaghan786@gmail.com>
2023-08-22 22:25:06 +0500

    Add files via upload

---
+++ javaStringCinversion.java
@@ -0,0 +1,12 @@
+public class javaStringCinversion {
+    
+    public static void main(String[] args) {
+        
+    String s = "200";
+
+    // 1st method using Integer.valueOf() method.
+    Integer i = Integer.valueOf(s);
+    System.out.println(i);  
+
+    }
+}
