21cda17ecf04b94dc42637752a97e842d86094ca
Maghan Das <dasmaghan786@gmail.com>
2023-12-30 22:20:18 +0500

    Add files via upload

---
+++ ExceptionHandlin.java
@@ -0,0 +1,15 @@
+
+// exception handling is powerful mechanism that is used to handle the runtime errors
+// so that the normal flow of program can be maintained.
+// In Java, an exception is an event that disrupts the normal flow of the program. 
+// It is an object which is thrown at runtime.
+// runtime errors such as ClassNotFoundException, IOException, SQLException,  etc.
+
+
+
+public class ExceptionHandlin {
+
+    public static void main(String[] args) {
+        
+    }
+}
