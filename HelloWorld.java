8fb208f4b849d22f59dae8c21db1a73e2b18cae1
Maghan Das <dasmaghan786@gmail.com>
2024-01-06 20:56:04 +0500

    helloWorld program

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+    
+
+    public static void main(String[] args) {
+        
+        System.out.println("hello world");
+        // declaring variable....
+        // variable is memory storage space for storing values...
+        // datatype--type of values that a variable can hold
+        int a = 10;
+         int b = 16;
+         System.out.println("the sum is " + a+ b);
+
+    }
+}
