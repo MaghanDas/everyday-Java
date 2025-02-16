7763f0e1afdd694e9efb3c881e41bbcab9db8551
Maghan Das <dasmaghan786@gmail.com>
2025-02-16 17:50:33 +0000

    Condtionals in java

---
+++ 02_conditionals/Conditionals.java
@@ -0,0 +1,25 @@
+
+public class Conditionals{
+
+  public static void main(String[] args){
+
+    /*
+  int age = 13;
+  String isEligibel = age >=18 ? "Eligibel" : "Not Eligibel";  
+
+   System.out.println(isEligibel); 
+
+   if(age >= 18)  {
+     System.out.println("is Eligible to vote!"); 
+   }
+    else 
+     System.out.println("not elgible to vote!");
+*/
+
+    int marks  = 90;
+    boolean isPassed = marks >= 33? true : false;
+    System.out.println(isPassed);
+
+    // switch statment code availbe in 01_basics folder..
+  }  
+}
