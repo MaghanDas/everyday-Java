4e16b00c72139d5157436ec902457046d387796d
Maghan Das <dasmaghan786@gmail.com>
2025-02-18 23:21:23 +0000

    implement linear search

---
+++ 07_LinearSearch/LinearSearch.java
@@ -0,0 +1,29 @@
+
+
+
+public class LinearSearch{
+  public static void main (String[] args){
+
+    // linear searching in array
+    // linear search is a searching algorithm that searches for an element in a list one by one.
+    // it is a sequential search algorithm.
+    // the best case time complexity of linear search is O(1) and worst case time complexity is O(n).
+    // the space complexity of linear search is O(1).
+
+    int[] arr = {1,2,3,4,5,6,7,8,9,10};
+    int target = 5;
+
+    int ans = linearSearch(arr, target);
+    System.out.println(ans);
+  }
+
+  static int linearSearch(int[] arr, int target){
+    for(int i = 0; i < arr.length; i++){
+      if(arr[i] == target){
+        return i;
+      }
+    }
+    return -1;
+  }
+
+}
