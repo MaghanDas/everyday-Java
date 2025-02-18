b7028ab9448a0349d43644edbc783a12dd3f1050
Maghan Das <dasmaghan786@gmail.com>
2025-02-18 23:20:33 +0000

    implement functions in java

---
+++ 06_functions/Functions.java
@@ -0,0 +1,15 @@
+
+public class Functions{
+
+  static void sum(int a, int b){
+    System.out.println(a + b);
+  }
+
+  public static void main(String[] args) {
+    
+    // sum(2,4);
+
+
+    
+  }
+}
