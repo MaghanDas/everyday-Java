fb7740d6179a2a4d94b06fe99e4a124712d93b25
Maghan Das <dasmaghan786@gmail.com>
2025-02-20 22:15:46 +0000

    sorting in java

---
+++ 09_Sorting/BubbleSort.java
@@ -0,0 +1,30 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 8, 4, 2};
+        bubbleSort(arr);
+        System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            // Compare adjacent elements up to the unsorted portion
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap elements
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            // Early exit if no swaps occurred (array is sorted)
+            if (!swapped) break;
+        }
+    }
+}
